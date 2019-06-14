c83be3f4abae2232e3ebad0dbfb7cc4277df018a
sdim-ua <ingersol@yandex.ua>
2019-06-14 14:37:05 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,28 @@
+/* 
+Чужой код, незнакомый
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet  {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
